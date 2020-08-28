f0ee2111251327c5537b56e95e0d4c5fcb6431d8
AraMatevosyan777 <60426627+AraMatevosyan777@users.noreply.github.com>
2020-08-28 15:50:57 +0400

    Updates

---
+++ precache-manifest.1bae389b2bf2a4b928b98a4eefddacdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17728270d433249916b29c07402a619",
+    "url": "/issues_page/index.html"
+  },
+  {
+    "revision": "7af717aed426398d0f38",
+    "url": "/issues_page/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "750bb639f22fe4fa399e",
+    "url": "/issues_page/static/css/main.4b9f3a28.chunk.css"
+  },
+  {
+    "revision": "7af717aed426398d0f38",
+    "url": "/issues_page/static/js/2.a43a6d0b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/issues_page/static/js/2.a43a6d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750bb639f22fe4fa399e",
+    "url": "/issues_page/static/js/main.bd23374f.chunk.js"
+  },
+  {
+    "revision": "15327e4c28fc776e2b09",
+    "url": "/issues_page/static/js/runtime-main.52e2b2f0.js"
+  }
+]);
